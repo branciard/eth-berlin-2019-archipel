12e3e6bd227468e43e5e75474e901297402142d0
Vladimir Ostapenco <vladost.pro@gmail.com>
2019-08-24 19:49:56 +0200

    Address correct

--- orchestrator/src/orchestrator.js
+++ orchestrator/src/orchestrator.js
@@ -33,28 +33,29 @@ const {
 const orchestrate = async () => {
 
   try {
-
+    // Converting string into arrays
     const hostsToPingArray = HOSTS_TO_PING.split(" ");
     const walletsArray = HOSTS_WALLETS.split(" ");
     
     console.log("Making pings to endpoints...");
-
     const pingResult = await pingEndpoints(hostsToPingArray);
   
     const failPings = pingResult.filter(element => element.reachable == false);
 
+    // If the node fails to ping all other nodes and external node so it is down
     if (failPings.length == HOSTS_TO_PING.split(" ").length) {
       console.log("Can't ping anyone. Change to sync.");
       await startSync(VALIDATOR_NAME);
 
     } else {
 
+      // Get current leader
       let currentLeader = await getLeader(ARCHIPEL_CONTRACT_ADDRESS, NODE_URL);
 
       console.log(currentLeader);
       console.log(ETH_ADDRESS);
 
-      if (currentLeader == 0) {
+      if (currentLeader == "0x0000000000000000000000000000000000000000") {
 
         const leadeshipChanged = await setLeader(PRIVATE_KEY, ARCHIPEL_CONTRACT_ADDRESS, NODE_URL, 0);
 
@@ -75,11 +76,13 @@ const orchestrate = async () => {
         const validatorStatus = pingResult[walletsArray.indexOf(currentLeader)]
 
         if (!validatorStatus.reachable) {
-          console.log("Validator is not avaliable. Starting validator node");
+
+          console.log("Validator is not reachable. Starting validator node");
           const leadeshipChanged = await setLeader(PRIVATE_KEY, ARCHIPEL_CONTRACT_ADDRESS, NODE_URL, currentLeader);
           if (leadeshipChanged) {
             await startValidator(VALIDATOR_NAME, VALIDATOR_KEY);
           }
+
         }
       }
     }
